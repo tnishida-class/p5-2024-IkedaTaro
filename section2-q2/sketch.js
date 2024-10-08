3b261f16148ef0c0c11656ae29a58a80b4996e14
IkedaTaro <147582731+IkedaTaro@users.noreply.github.com>
2024-10-08 12:18:53 +0900

    section 2-q1,2,3 finished

--- section2-q2/sketch.js
+++ section2-q2/sketch.js
@@ -1,11 +1,28 @@
 // チェッカー
 function setup() {
   createCanvas(200, 200);
-  let size = width / 8;
+  background(255);
+
+  let size = width / 8; //widthはキャンバスの幅　つまり200
   noStroke();
   for(let i = 0; i < 8; i++){
     for(let j = 0; j < 8; j++){
+      let x = size * i;
+      let y = size * j;
+      if ((i + j) % 2 === 1) {
+        fill(122);
+        rect(x,y,size,size);
+      }
+      if (((i + j) % 2 === 1) && (j < 3)) {
+        fill(255, 0, 0);
+        ellipse(x + size / 2, y + size / 2, size); 
+      }
+
+      if (((i + j) % 2 === 1) && (4 < j)) {
+        fill(0);
+        ellipse(x + size / 2, y + size / 2, size, size);
+      }
+        } 
       // BLANK[1] (hint: rectのx座標は size * i, y座標はsize * j)
     }
   }
-}
